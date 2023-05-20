450b61e603d6358e09e2c96a7bc1163fff125715
DmytroErmakov <demchik22@gmail.com>
2023-05-20 23:55:40 +0300

    run task01

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,34 @@
+let nIntervId;
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stoptBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.stoptBtn.setAttribute('disabled', true);
+refs.startBtn.addEventListener('click', onStartBtnChangeColor);
+refs.stoptBtn.addEventListener('click', onStoptBtnChangeColor);
+
+function onStartBtnChangeColor() {
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stoptBtn.removeAttribute('disabled');
+
+  nIntervId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStoptBtnChangeColor() {
+  refs.stoptBtn.setAttribute('disabled', true);
+  refs.startBtn.removeAttribute('disabled');
+
+  clearInterval(nIntervId);
+  nIntervId = null;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
